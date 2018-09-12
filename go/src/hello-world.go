8c221107f439f29c208d9a871d63c5b4e5a831c6
Dheeraj Reddy Pailla <dheerajpreddy@github.com>
2018-09-12 08:04:48 +0530

    Added Hello World program

---
+++ go/src/hello-world.go
@@ -0,0 +1,10 @@
+// Naming this your main package, if this isn't named main, code won't run
+package main
+
+// Importing the fmt package to print to terminal
+import "fmt"
+
+// defining main function
+func main() {
+    fmt.Printf("hello, world\n")
+}
