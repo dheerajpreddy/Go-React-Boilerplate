06d754c4585fa4519fee5fcfc4d2f9e3dcb1301d
Dheeraj Reddy Pailla <dheerajpreddy@github.com>
2018-09-13 02:57:53 +0530

    Built up react app to route through pages

---
+++ react-app/src/components/DeletePerson.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './DeletePerson.css';
+
+class DeletePerson extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Delete a Person</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default DeletePerson;
---
+++ react-app/src/components/EditPerson.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './EditPerson.css';
+
+class EditPerson extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Edit a Person</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default EditPerson;
