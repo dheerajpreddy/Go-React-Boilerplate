216dee9d2faf54b92e34bbb839708863cd2173de
Dheeraj Reddy Pailla <dheerajpreddy@gmail.com>
2018-09-12 14:00:55 +0530

    Added comments everywhere

--- go/src/4 - Using an ORM.go
+++ go/src/4 - Using an ORM.go
@@ -2,10 +2,11 @@ package main
 
 import (
   "fmt"
-  "github.com/jinzhu/gorm"
-  _ "github.com/jinzhu/gorm/dialects/sqlite"
+  "github.com/jinzhu/gorm"                            // ORM package for Go
+  _ "github.com/jinzhu/gorm/dialects/sqlite"          // for SQLite. Only imports functions so that ORM can use. Hence the '_'
 )
 
+// This is the structure for your database. Very similar to how SQLAlchemy works with Flask.
 type Person struct {
   ID uint `json:"id"`
   FirstName string `json:"firstname"`
@@ -13,15 +14,14 @@ type Person struct {
 }
 
 func main() {
-   db, _ := gorm.Open("sqlite3", "./gorm.db")
-   defer db.Close()
-   db.AutoMigrate(&Person{})
+   db, _ := gorm.Open("sqlite3", "./gorm.db")             // Creates an SQLite database, stores it in the file.
+   defer db.Close()                                       // defer basically tells it to execute at the end of the main function's scope
+   db.AutoMigrate(&Person{})                              // Creates database based on the Person structure as schema
    p1 := Person{FirstName: "John", LastName: "Doe"}
-   p2 := Person{FirstName: "Jane", LastName: "Smith"}
-   db.Create(&p1)
-   var p3 Person // identify a Person type for us to store the results in
-   db.First(&p3) // Find the first record in the Database and store it in p3
-   fmt.Println(p1.FirstName)
-   fmt.Println(p2.LastName)
-   fmt.Println(p3.LastName) // print out our record from the database
+   p2 := Person{FirstName: "Jane", LastName: "Smith"}     // Example Person objects being created
+   db.Create(&p1)                                         // Creates an entry in the db with the object p1
+   var p3 Person                                          // identify a Person type for us to store the results in
+   db.First(&p3)                                          // Find the first record in the Database and store it in p3
+   fmt.Println(p3.FirstName)
+   fmt.Println(p3.LastName)                               // print out our record from the database
 }
