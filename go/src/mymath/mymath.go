ad1b478827de409267bbefae6d3bb3d64270a7e4
Dheeraj Reddy Pailla <dheerajpreddy@github.com>
2018-09-12 10:45:26 +0530

    Added package usage code

---
+++ go/src/mymath/mymath.go
@@ -0,0 +1,25 @@
+// Naming this package
+package mymath
+
+// Go is strongly typed (Unlike Python, and like C). Notice types are declared after variable names.
+// The return type is declared after parameters are specified.
+
+// defining sum function
+func Sum(x int, y int) int {
+	return x + y
+}
+
+// defining diff function
+func Diff(x int, y int) int {
+	return x - y
+}
+
+// defining diff function
+func Mul(x int, y int) int {
+	return x - y
+}
+
+// defining diff function
+func Div(x float64, y float64) float64 {
+  return (x / y)
+}
