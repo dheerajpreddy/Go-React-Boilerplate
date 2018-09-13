506156596ff83d8760f28bd4d7749dfd9198f136
Dheeraj Reddy Pailla <dheerajpreddy@gmail.com>
2018-09-14 04:33:11 +0530

    Updated readme

--- go/src/5 - CRUD API.go
+++ go/src/5 - CRUD API.go
@@ -2,7 +2,7 @@ package main
 
 import (
    "fmt"
-   "github.com/gin-contrib/cors"
+   "github.com/gin-contrib/cors"                        // Why do we need this package?
    "github.com/gin-gonic/gin"
    "github.com/jinzhu/gorm"
    _ "github.com/jinzhu/gorm/dialects/sqlite"           // If you want to use mysql or any other db, replace this line
